ae633948a3c9fd573dbdeffb2057f31a2a3b2596
Faizaan-J <125227430+Faizaan-J@users.noreply.github.com>
2026-05-10 22:30:25 -0400

    Initial commit

---
+++ api/users/detailed-user-info.ts
@@ -0,0 +1,11 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+const handler = async (request: VercelRequest, response: VercelResponse) => {
+  response.status(200).json({
+    body: 'Hiii',
+    query: request.query,
+    cookies: request.cookies,
+  });
+}
+
+export default handler;
