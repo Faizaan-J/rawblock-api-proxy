1a0b269e98b5cb41c597f5952bc3f7d4b1e659f3
Faizaan J <125227430+Faizaan-J@users.noreply.github.com>
2026-05-14 21:20:51 -0400

    FEAT: Implement detailed user info API

--- api/users/detailed-user-info.ts
+++ api/users/detailed-user-info.ts
@@ -1,8 +1,11 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 const handler = async (request: VercelRequest, response: VercelResponse) => {
+  const { userId } = request.query;
+  const userInfo = await fetch(`https://users.roblox.com/v1/users/${userId}/`);
+
   response.status(200).json({
-    body: 'Success',
+    body: userInfo.body,
     query: request.query,
     cookies: request.cookies,
   });
