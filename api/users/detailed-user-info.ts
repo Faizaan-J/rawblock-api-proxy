8059629c3e1ed4ec4d6669d6cd2576c0d9a13265
Faizaan J <125227430+Faizaan-J@users.noreply.github.com>
2026-05-14 21:38:54 -0400

    fix yo

--- api/users/detailed-user-info.ts
+++ api/users/detailed-user-info.ts
@@ -2,10 +2,11 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 const handler = async (request: VercelRequest, response: VercelResponse) => {
   const { userId } = request.query;
-  const userInfo = await fetch(`https://users.roblox.com/v1/users/${userId}/`);
+  const fetchResponse = await fetch(`https://users.roblox.com/v1/users/${userId}/`);
+  const userInfo = await fetchResponse.json();
 
   response.status(200).json({
-    body: userInfo.body,
+    body: userInfo,
     query: request.query,
     cookies: request.cookies,
   });
