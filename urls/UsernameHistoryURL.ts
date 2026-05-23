1438864815336640960a4de7b00b4003fb9f212d
Faizaan-J <125227430+Faizaan-J@users.noreply.github.com>
2026-05-23 13:27:07 -0400

    FIX: Set default limit in username-history to an allowed one

--- urls/UsernameHistoryURL.ts
+++ urls/UsernameHistoryURL.ts
@@ -3,7 +3,7 @@ import URLCLass from "../classes/URLClass";
 type SortOrderType = "Asc" | "Desc";
 
 class UsernameHistoryURL extends URLCLass {
-    generate(userId: number, limit : number = 20, sortOrder : SortOrderType = "Asc", nextCursor?: string | undefined) : URL {
+    generate(userId: number, limit : number = 10, sortOrder : SortOrderType = "Asc", nextCursor?: string | undefined) : URL {
         const url = new URL(`https://users.roblox.com/v1/users/${userId}/username-history`);
         url.searchParams.append("limit", limit.toString());
         url.searchParams.append("sortOrder", sortOrder);
