6d3a09df5f8f7a6fef6bae4da1b3f76008999c35
Faizaan-J <125227430+Faizaan-J@users.noreply.github.com>
2026-05-23 13:27:29 -0400

    REFACTOR: Rename nextCursor to cursor for disambiguation

--- urls/UsernameHistoryURL.ts
+++ urls/UsernameHistoryURL.ts
@@ -3,12 +3,12 @@ import URLCLass from "../classes/URLClass";
 type SortOrderType = "Asc" | "Desc";
 
 class UsernameHistoryURL extends URLCLass {
-    generate(userId: number, limit : number = 10, sortOrder : SortOrderType = "Asc", nextCursor?: string | undefined) : URL {
+    generate(userId: number, limit : number = 10, sortOrder : SortOrderType = "Asc", cursor?: string | undefined) : URL {
         const url = new URL(`https://users.roblox.com/v1/users/${userId}/username-history`);
         url.searchParams.append("limit", limit.toString());
         url.searchParams.append("sortOrder", sortOrder);
-        if (nextCursor != undefined && nextCursor != "") {
-            url.searchParams.append("cursor", nextCursor);
+        if (cursor != undefined && cursor != "") {
+            url.searchParams.append("cursor", cursor);
         }
         return url;
     }
