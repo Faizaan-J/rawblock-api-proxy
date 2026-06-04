f9665c0458f1cb7ad1db14f99f841ba1c019365c
Faizaan-J <125227430+Faizaan-J@users.noreply.github.com>
2026-06-04 19:27:16 -0400

    FEAT: Implement zod for validating request queries

--- api/users/username-history.ts
+++ api/users/username-history.ts
@@ -1,19 +1,21 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import UsernameHistoryURL from '../../urls/UsernameHistoryURL';
 
+import * as zod from "zod";
+
 const handler = async (request: VercelRequest, response: VercelResponse) => {
-    const query = request.query;
-    const userId = String(query.userId || '');
-    const limit = query.limit ? Number(query.limit) : undefined;
-    const sortOrder = (() => {
-        const param = String(query.sortOrder).toLowerCase();
-        if (param === "asc") return "Asc";
-        if (param === "desc") return "Desc";
-    })();
-    const cursor = String(query.cursor || '');
+    const urlResult = UsernameHistoryURL.generate(request.query);
+    
+    if (!(urlResult instanceof URL)) {
+        response.status(400).json({
+            error: "Invalid query parameters",
+            details: urlResult,
+        });
+        return;
+    }
 
     const fetchedUsernameHistoryPage = 
-        await fetch(UsernameHistoryURL.generate(Number(userId), limit, sortOrder, cursor))
+        await fetch(urlResult)
             .then(res => res.json());
 
     console.log(fetchedUsernameHistoryPage, "fetchedUsernameHistoryPage");
